d9f1cfc5a96db3f0009152d275000bbdb4173164
Akito-Shirai <tnbc080527@gmail.com>
2024-12-20 07:02:16 +0900

    Update files.

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -1,15 +1,19 @@
 package com.techacademy;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InputController {
 
     @GetMapping("/input")
-    public String getInput() {
+    public String getInput(@RequestParam(value="previous", required = false, defaultValue = "") String previous, Model model) {
+        if (!previous.isEmpty()) {
+            model.addAttribute("previous", previous);
+        }
         return "input";
     }
-
 
 }
